3ca4cdc3dc5b1e04ae414c1514cbf5feb98cbe84
Michael Luich <mluich@stonesrose.com>
2016-05-10 01:01:10 +0000

    20160509 MJL blah

---
+++ opscomm/views.py
@@ -0,0 +1,5 @@
+from rest_framework import viewsets
+
+#class opscommViewSet(viewsets.ModelViewSet)
+def opscommViewSet(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
